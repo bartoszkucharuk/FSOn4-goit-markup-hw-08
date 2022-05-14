ebc801819c671a23bd1403c35026659704d10da6
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2022-05-14 12:56:13 +0200

    .
    .

--- js/mobile.js
+++ js/mobile.js
@@ -1,12 +1,12 @@
 (() => {
   const refs = {
-    openMobileBtn: document.querySelector('[data-mobile-open]'),
-    closeMobileBtn: document.querySelector('[data-mobile-close]'),
+    openMenuBtn: document.querySelector('[data-mobile-open]'),
+    closeMenuBtn: document.querySelector('[data-mobilec-lose]'),
     modal: document.querySelector('[data-mobile]'),
   };
 
-  refs.openMobileBtn.addEventListener('click', toggleModal);
-  refs.closeMobileBtn.addEventListener('click', toggleModal);
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
